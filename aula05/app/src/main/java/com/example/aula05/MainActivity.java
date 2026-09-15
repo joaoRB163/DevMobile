bf4de5696b786c151ca9e7d26412f6061d6ebfa7
joaoRB163 <joaobataglion087@gmail.com>
2026-09-14 21:50:02 -0300

    mostrar elementos no ListView

--- aula05/app/src/main/java/com/example/aula05/MainActivity.java
+++ aula05/app/src/main/java/com/example/aula05/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aula05;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] nomes = new String[]{"Helena", "Lívia", "Rômulo", "Pedro", "Jão", "Aureo"};
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lv = findViewById(R.id.listview);
+
+        // Criação do adaptador (quem vai construir a informação)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1, // layout pronto do SDK. CTRL + Botão esquerdo abre o layout
+                android.R.id.text1, // elemento que vai receber a informação
+                nomes // array com elementos (dados)
+                );
+        // colocando o adaptador na listview
+        lv.setAdapter(adapter);
     }
 }
