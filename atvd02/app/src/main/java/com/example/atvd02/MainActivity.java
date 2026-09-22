9ce1cdb2156e17610251eaceaf6960fd2bfe3e19
joaoRB163 <joaobataglion087@gmail.com>
2026-09-21 21:17:57 -0300

    começo de um sonho

--- atvd02/app/src/main/java/com/example/atvd02/MainActivity.java
+++ atvd02/app/src/main/java/com/example/atvd02/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.atvd02;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +17,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    PackageManager packageManager = getPackageManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        lv = findViewById(R.id.listview_apps);
+
+        /* Cria um Intent com a ação ACTION_MAIN, que é usada para filtrar aplicativos
+           que tenham uma atividade principal. O segundo argumento null indica que não há
+           um componente específico a ser direcionado neste momento.*/
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+
+        List<ApplicationInfo> packageInfoList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        List<ResolveInfo> appList = packageManager.queryIntentActivities(mainIntent, 0);
+
+        AppAdapter appAdapter = new AppAdapter(this,R.layout.item_lista, packageInfoList);
+        lv.setAdapter(appAdapter);
+
+
+
     }
+
+
+
 }
